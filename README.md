401fc6ed9db1f942a5821254b77043f346ef7148
Benjamin Sternthal <ben@devpatch.com>
2017-04-04 20:58:37 -0700

    initial check in

---
+++ README.md
@@ -0,0 +1,15 @@
+1. run ```npm install```
+1. In app.js populate:
+ 	1. username
+	2. password
+	3. path to csv file
+1. Create directory /log in project
+1. Create log files in /log (I should do this auto-magically but this is a rush)
+	1. failureLog.txt
+	2. requestErrorLog.txt
+	3. successLog.txt
+
+Test.csv
+1. First result should generate request error.
+1. Second result should generate url match failure.
+1. Thirs result should work
---
+++ app.js
@@ -0,0 +1,71 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var request = require('request');
+var sleep = require('system-sleep');
+
+var urls = new Array();
+var csvFile = './csv/test.csv';
+var failureLog = './logs/failureLog.txt';
+var requestErrorLog = './logs/requestErrorLog.txt';
+var successLog = './logs/successLog.txt';
+var sleepDelayMS = 2000;
+
+/* Diable SSL Checking GLobally */
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+/* Options for HTTP Get Request, remove Auth if you are not testing stage */
+var requestOptions = {
+    auth: {
+        username: '',
+        password: ''
+    },
+    followAllRedirects: 'true'
+};
+
+
+/* Receives a file and parses based on supplied delimeter, 
+ * for each line passes 2 urls (original & final) to be tested. */ 
+var parser = parse({delimiter: ','}, function(err, data){
+    if (err) {
+        console.error('Error: ', err);
+        return;
+    }
+    
+    data.forEach(function(row) {
+        // URLS To Test, eh basing this on ruby version... csv file should be cleaner.
+        var originalURL = row[3];
+        var finalURL = row[6];
+        
+        // pass urls to request but sleep for N milliseconds to not kill the server.
+        sleep(sleepDelayMS);
+        //Show we are doing something
+        process.stdout.write(".");
+        testURL(originalURL, finalURL);
+    });
+});
+
+
+ /* Requests original url and stores response following redirects */
+function testURL(originalURL, finalURL) {
+    requestOptions.url = originalURL;
+    
+    request(requestOptions, function (error, response, body) {
+        if (error) {
+            fs.appendFileSync(requestErrorLog, error + ' ' + originalURL + "\n");
+        } else {
+            writeResults(response, originalURL, finalURL);
+        }     
+    });    
+}
+
+/* if Final URL Matches URL (after all the redirects) record success if not record failure */
+function writeResults(response, originalURL, finalURL) {
+    if(response.request.uri.href === finalURL) {
+        fs.appendFileSync(successLog, 'Success: ' + originalURL + " , " + finalURL + "\n");        
+    } else {
+        fs.appendFileSync(failureLog, 'Error: ' + originalURL + " , " + finalURL + " , " + response.request.uri.href + '\n'); 
+    }
+}
+
+// Read CSV File, kicks off everything
+fs.createReadStream(csvFile).pipe(parser);
